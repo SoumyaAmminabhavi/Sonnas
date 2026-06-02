138fe3cd9efbacf0838e699a3b6415214bce6e24
SoumyaAmminabhavi <soumyaamminabhavi@gmail.com>
2026-06-02 16:52:42 +0530

    fix: create dummy .env file during Vercel build

--- vercel-build.sh
+++ vercel-build.sh
@@ -3,5 +3,8 @@ echo "Downloading Flutter..."
 git clone https://github.com/flutter/flutter.git -b stable --depth 1
 export PATH="$PATH:`pwd`/flutter/bin"
 
+echo "Creating dummy .env file for Flutter..."
+touch .env
+
 echo "Building Flutter Web App..."
 flutter build web --release
